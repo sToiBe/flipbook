d7185ecca8e4132d3f53e21e6ff9be05a3a0eb40
sToiBe <diazvaldez097@gmail.com>
2025-05-26 16:14:04 -0300

    Simplify className declarations in App component
    Removed redundant padding class in outer div and unnecessary background class in the inner div. This improves code readability and maintains the layout functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
     return (
         <>
             <div
-                className="flex justify-center items-center p-4 sm:p-8 w-screen h-screen overflow-hidden">
-                <div className="w-full h-full bg-red-50 flex justify-center items-center">
+                className="flex justify-center items-center sm:p-8 w-screen h-screen overflow-hidden">
+                <div className="w-full h-full flex justify-center items-center">
                     <Book/>
                 </div>
             </div>
